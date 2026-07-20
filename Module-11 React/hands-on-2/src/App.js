7a76f9dd76481a9c4dcc3289a1f5096b664e478a
K Lasya Rao <155871649+Lasya2219@users.noreply.github.com>
2026-07-20 10:36:36 +0530

    Add files via upload

---
+++ Module-11 React/hands-on-2/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Home from './Components/Home.js';
+import About from './Components/About.js';
+import Contact from './Components/Contact.js';
+
+function App() {
+  return (
+    <div className='background'>
+      <Home/>
+      <About/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
